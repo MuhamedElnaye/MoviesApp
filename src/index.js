bd44e1136a181494201553d62687ce209233c217
muhamedelnaye <muh22199922@gmail.com>
2023-04-23 22:42:03 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={"/MoviesApp"}>
       <App />
     </Router>
   </React.StrictMode>
